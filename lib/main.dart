37701c7968a52fdb3d636ce489e1f5bc20bcaa81
agatha-chituwa <bsc-com-06-18@cc.ac.mw>
2024-03-08 11:14:53 +0200

    n

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:geni_app/login/VerifyLogin.dart';
 import 'package:geni_app/ui/slash.dart';
 
 main() {
@@ -26,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const VerifyLogin(),
+      home: const Slash(),
     );
   }
 }
